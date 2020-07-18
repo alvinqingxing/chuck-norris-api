1a3edd7d4f72b122389f930fec3d02face0cc6b1
Alvin Lim <alvinch@hawaii.edu>
2020-07-18 17:29:23 +0800

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'alvinlim.me'
+    origins 'alvinqingxing.github.io'
     resource '*',
       credentials: true,
       headers: :any,
